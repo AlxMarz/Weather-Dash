e1c31fabaa62ddddc612d3081251cc5c0137fc50
alex marquez <marquez798@gmail.com>
2023-04-25 10:22:59 -0500

    added event listenr, api call

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-var APIkey = "7eb5330d2f0977e81f7f2aaf26f016e6"
+var APIKey = "7eb5330d2f0977e81f7f2aaf26f016e6"
+
+
+//event listener form the button of search 
+$('city').click(function (event){
+    event.preventDefault()
+    event.stopPropagation()
+
+    var city = ('#city').val.trim
+
+})
+
+//get weather 
+function getWeather (city){
+    var forecastURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=" + APIKey;
+
+
+
+    var lat, lon
+
+
+}
