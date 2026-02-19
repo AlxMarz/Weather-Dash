da1df43fb73a11b832221fc48b4a5ea8dce306fd
alex marquez <marquez798@gmail.com>
2026-02-18 21:33:18 -0600

    w

--- script.js
+++ script.js
@@ -1,167 +1,152 @@
-var APIKey = "7eb5330d2f0977e81f7f2aaf26f016e6"
-var fetchButton = document.getElementById("fetch-button")
-var geoCall = "https://api.openweathermap.org/geo/1.0/direct?q="
-var searchField = document.getElementById("search-field")
-var apiExclude = "&exclude=minutely,hourly,alerts"
-var apiCall = "https://api.openweathermap.org/data/2.5/onecall?"
-var citySearch = document.getElementById("searchCity")
-
-
-
-
+var APIKey = "7eb5330d2f0977e81f7f2aaf26f016e6";
+var fetchButton = document.getElementById("fetch-button");
+var geoCall = "https://api.openweathermap.org/geo/1.0/direct?q=";
+var searchField = document.getElementById("search-field");
+var apiExclude = "&exclude=minutely,hourly,alerts";
+var apiCall = "https://api.openweathermap.org/data/2.5/onecall?";
+var citySearch = document.getElementById("searchCity");
 
 // Dom load
 
-$(document).ready(function () {
+$(document).ready(function () {});
 
-})
+//daily weather
+function renderWeather(weather) {
+  //daily
+  console.log(weather);
+  // space
+  var resultsContainer = document.querySelector("#weather-results");
+  // create h2 for name
+  var city = document.createElement("h2");
+  city.textContent = weather.name;
+  resultsContainer.append(city);
+  // create p for humidity, wind, description, temp (time)
+  var temp = document.createElement("p");
+  temp.textContent = "Temp: " + weather.main.temp + " F";
+  resultsContainer.append(temp);
 
+  var humidity = document.createElement("p");
+  humidity.textContent = "humidity: " + weather.main.humidity + " %";
+  resultsContainer.append(humidity);
 
+  var wind = document.createElement("p");
+  wind.textContent =
+    "wind: " + weather.wind.speed + " mph, " + weather.wind.deg + "°";
+  resultsContainer.append(wind);
 
-//daily weather 
-function renderWeather(weather){
-    //daily
-    console.log(weather);
-    var resultsContainer = document.querySelector("#weather-results")
-    // create h2 for name
-    var city = document.createElement("h2")
-    city.textContent = weather.name 
-    resultsContainer.append(city)
-    // create p for humidity, wind, description, temp (time)
-    var temp = document.createElement("p")
-    temp.textContent = "Temp: " + weather.main.temp + " F"
-    resultsContainer.append(temp)
-
-    var humidity = document.createElement ("p")
-    humidity.textContent = "humidity: " + weather.main.humidity + " %"
-    resultsContainer.append(humidity)
-    
-    var wind = document.createElement("p")
-    wind.textContent = "wind: " + weather.wind.speed + " mph, " + weather.wind.deg + "°"
-    resultsContainer.append(wind)
-
-    var weatherDetails = weather.weather[0] 
-    if (weatherDetails &&  weatherDetails.description){
-        var description = document.createElement("p")
-        description.textContent = weatherDetails.description
-        resultsContainer.append(description)
-    }
-
-
+  var weatherDetails = weather.weather[0];
+  if (weatherDetails && weatherDetails.description) {
+    var description = document.createElement("p");
+    description.textContent = weatherDetails.description;
+    resultsContainer.append(description);
+  }
 }
-
-
 
 function fetchWeather(query) {
-    var url = 
+  var url =
     "https://api.openweathermap.org/data/2.5/weather?q=" +
-    query + 
-    "&units=imperial&appid=7eb5330d2f0977e81f7f2aaf26f016e6"
+    query +
+    "&units=imperial&appid=7eb5330d2f0977e81f7f2aaf26f016e6";
 
-    fetch(url)
-        .then((response) => response.json())
-        .then((data) => renderWeather(data))
-        
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => renderWeather(data));
 }
-
 
 function fetchForecast(query) {
-    // var url = 
-    // "https://api.openweathermap.org/data/2.5/weather?q=" +
-    // query + 
-    // "&units=imperial&appid=7eb5330d2f0977e81f7f2aaf26f016e6"
-    
+  // var url =
+  // "https://api.openweathermap.org/data/2.5/weather?q=" +
+  // query +
+  // "&units=imperial&appid=7eb5330d2f0977e81f7f2aaf26f016e6"
 
-    var url = "https://api.openweathermap.org/data/2.5/forecast?q=" + query +    
-     "&units=imperial&appid=" + APIKey
+  var url =
+    "https://api.openweathermap.org/data/2.5/forecast?q=" +
+    query +
+    "&units=imperial&appid=" +
+    APIKey;
 
+  fetch(url)
+    .then((response) => response.json())
+    //forecast
+    .then(function (response) {
+      console.log(response);
 
-    fetch(url)
-        .then((response) => response.json())
-        //forecast
-        .then(function (response) {
-            console.log(response)
+      var header = $("<h4>")
+        .attr("class", "card-header p-2 mt-4 shadow")
+        .text("5-Day Forecast:");
 
-            var header = $("<h4>").attr("class", "card-header p-2 mt-4 shadow").text("5-Day Forecast:")
+      $("#five").prepend(header);
 
-            $("#five").prepend(header)
+      // Five day forecast
 
-                // Five day forecast
+      $.each(response.list, function (i, day) {
+        var time = moment(day.dt_txt).hour();
 
-            $.each(response.list, function (i, day) {
+        console.log(i, day.dt_txt, time);
+        if (time == 15) {
+          var card = $("<div>").attr(
+            "class",
+            "card text-white text-center bg-primary m-2 shadow",
+          );
 
+          var date = $("<div>")
+            .text(moment(day.dt_txt).format("ddd, Do"))
+            .attr("class", "card-header");
+          var body = $("<div>").attr("class", "card-body");
+          var img = $("<img>").attr(
+            "src",
+            "http://openweathermap.org/img/wn/" +
+              day.weather[0].icon +
+              "@2x.png",
+          );
+          // c.append(img)
+          var temp = $("<div>")
+            .text("Temp: " + day.main.temp + " F")
+            .attr("class", "card-text ");
+          var hum = $("<div>")
+            .text("Humidity: " + day.main.humidity + "%")
+            .attr("class", "card-text ");
 
-                var time = moment(day.dt_txt).hour()
-
-                console.log(i, day.dt_txt, time)
-                if (time == 15) {
-                    var card = $("<div>").attr("class", "card text-white text-center bg-primary m-2 shadow")
-
-                    var date = $("<div>").text(moment(day.dt_txt).format("ddd, Do")).attr("class", "card-header")
-                    var body = $("<div>").attr("class", "card-body")
-                    var img = $("<img>").attr("src", "http://openweathermap.org/img/wn/" + day.weather[0].icon + "@2x.png")
-                    // c.append(img)
-                    var temp = $("<div>").text("Temp: " + day.main.temp + " F").attr("class", "card-text ")
-                    var hum = $("<div>").text("Humidity: " + day.main.humidity + "%").attr("class", "card-text ")
-
-                    body.append(img, temp, hum)
-                    card.append(date, body)
-                    $("#foreCast").append(card)
-
-
-                }
-
-
-            })
-
-
-
-        })
-
-
+          body.append(img, temp, hum);
+          card.append(date, body);
+          $("#foreCast").append(card);
+        }
+      });
+    });
 }
 
-// fetchWeather("london")
+$("#fetch-button").click(function (event) {
+  event.preventDefault();
+  event.stopPropagation();
 
-$('#fetch-button').click(function (event){
+  // Put city into varaible
+  var searchCity = $("#search-field").val().trim();
+  console.log($(this).text());
 
-    event.preventDefault()
-    event.stopPropagation()
+  // fetch city
 
-    // Put city into varaible
-    var searchCity = $('#search-field').val().trim()
-    console.log($(this).text())
+  fetchWeather(searchCity);
 
-    // fetch city
+  fetchForecast(searchCity);
 
-    fetchWeather(searchCity)
-
-    fetchForecast(searchCity)
-
-
-    // console.log(searchCity)
-
-})
-
-
-
-
-
-
-
+  // console.log(searchCity)
+  function clearResults() {
+    var resultsContainer = document.querySelector("#weather-results");
+    resultsContainer.innerHTML = "";
+    // Source - https://stackoverflow.com/a/6798187
+    // Posted by alex, modified by community. See post 'Timeline' for change history
+    // Retrieved 2026-02-18, License - CC BY-SA 3.0
+    var forecastContainer = document.querySelector('#five')
+    forecastContainer.innerHTML = ""
+  }
+  clearResults();
+});
 
 //5 day
 
-
-    
-
-
-
-
-
 // //gets lat lon from city user typed in
 // function getLatLon(query){
-//     //gets city from text intput 
+//     //gets city from text intput
 //     var city = searchField.val().trim()
 //     fetch(geoCall+city+apiExclude+APIKey)
 //         .then(function(response){
@@ -170,13 +155,13 @@ $('#fetch-button').click(function (event){
 //         .then (function(data){
 //             var cityLat = "lat="+data[0].lat
 //             var cityLon = "&lon"+data[0].lon
-//             //takes city lat and lon then sends it get forecast 
+//             //takes city lat and lon then sends it get forecast
 //             getCityData(cityLat, cityLon, city)
 //             console.log(data);
 //         })
 // }
 
-// //uses city lat and lon to get the forecast 
+// //uses city lat and lon to get the forecast
 // function getCityData(cityLat,cityLon,city){
 //     fetch(apiCall+cityLat+cityLon+"&units=imperial"+apiExclude+APIKey)
 //     .then(function(data){
@@ -186,11 +171,7 @@ $('#fetch-button').click(function (event){
 //     })
 // }
 
-
 // fetchButton.addEventListener('click', getLatLon)
-
-
-
 
 // function getApi(){
 //     var requestUrl = "http://api.openweathermap.org/geo/1.0/direct?q=kansascity&limit=5&appid=7eb5330d2f0977e81f7f2aaf26f016e6"
@@ -203,7 +184,7 @@ $('#fetch-button').click(function (event){
 //         })
 // }
 
-// //event listener form the button of search 
+// //event listener form the button of search
 // $('city').click(function (event){
 //     event.preventDefault()
 //     event.stopPropagation()
@@ -212,13 +193,10 @@ $('#fetch-button').click(function (event){
 
 // })
 
-// //get weather 
+// //get weather
 // function getWeather (city){
 //     var forecastURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=" + APIKey;
 
-
-
 //     var lat, lon
 
-
-// }
+//
